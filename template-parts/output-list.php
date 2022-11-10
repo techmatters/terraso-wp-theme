06d7134a993cfa36cc2d15a30e1a95a027cd0f31
Paul Schreiber <paul@techmatters.org>
2022-11-10 17:14:08 -0500

    feat:add title inside output list

--- template-parts/output-list.php
+++ template-parts/output-list.php
@@ -13,39 +13,41 @@ $outputs = get_children( // phpcs:ignore WordPressVIPMinimum.Functions.Restricte
 	]
 );
 ?>
-<h2><?php echo esc_html( $args['title'] . ' Outputs' ); ?></h2>
-
-<div class='output-wrapper'>
-<?php
-foreach ( $outputs as $output ) {
-	$tools           = get_children( // phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.get_posts_get_children
-		[
-			'post_parent' => $output->ID,
-			'post_type'   => ILM_Guide::POST_TYPE,
-			'numberposts' => ILM_Guide::POST_LIMIT, // phpcs:ignore WordPress.WP.PostsPerPage.posts_per_page_numberposts
-		]
-	);
-	$first_paragraph = str_replace( '</p>', ' <a href="' . esc_url( get_permalink( $output->ID ) ) . '">Read More</a></p>', ILM_Guide::get_first_paragraph( $output->post_content ) );
-	?>
-
-<section class='output'>
-	<h3><?php echo esc_html( $output->post_title ); ?></h3>
-	<?php echo wp_kses_post( $first_paragraph ); ?>
-
-	<details>
-		<summary><h4>Suggested tools that can help achieve this output</h4></summary>
-		<ol>
-
+<article class='guide-outputs'>
+	<h2><?php echo esc_html( $args['title'] . ' Outputs' ); ?></h2>
+	<div class='output-wrapper'>
 	<?php
-	foreach ( $tools as $tool ) {
+	foreach ( $outputs as $output ) {
+		$tools           = get_children( // phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.get_posts_get_children
+			[
+				'post_parent' => $output->ID,
+				'post_type'   => ILM_Guide::POST_TYPE,
+				'numberposts' => ILM_Guide::POST_LIMIT, // phpcs:ignore WordPress.WP.PostsPerPage.posts_per_page_numberposts
+			]
+		);
+		$output_link = get_permalink( $output->ID );
+		$first_paragraph = str_replace( '</p>', ' <a class="read-more" href="' . esc_url( $output_link ) . '">Read More</a></p>', ILM_Guide::get_first_paragraph( $output->post_content ) );
 		?>
-				<li><a href="<?php echo esc_url( get_the_permalink( $tool->ID ) ); ?>'"><?php echo esc_html( $tool->post_title ); ?></a></li>
-			<?php
-	}
-	?>
 
-				</ol>
-	</details>
-</section>
-<?php } // section ?>
-</div>
+	<section class='output'>
+		<h3><a href="<?php echo esc_url( $output_link ); ?>"><?php echo esc_html( $output->post_title ); ?></a></h3>
+		<?php echo wp_kses_post( $first_paragraph ); ?>
+
+		<details>
+			<summary><h4>Suggested tools that can help achieve this output</h4></summary>
+			<ol>
+
+		<?php
+		foreach ( $tools as $tool ) {
+			?>
+					<li><a href="<?php echo esc_url( get_the_permalink( $tool->ID ) ); ?>"><?php echo esc_html( $tool->post_title ); ?></a></li>
+				<?php
+		}
+		?>
+
+					</ol>
+		</details>
+	</section>
+	<?php } // section ?>
+	</div>
+</article>
