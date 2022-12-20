47c42d36e008b9aab765b2c2b5ee0774857ec45a
Paul Schreiber <paulschreiber@gmail.com>
2022-12-20 15:15:04 -0500

    style: eslint js file

--- assets/js/src/plausible.js
+++ assets/js/src/plausible.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', addPlausibleTracking);
+document.addEventListener( 'DOMContentLoaded', addPlausibleTracking );
 
 function addPlausibleTracking() {
-  if (!document.body.classList.contains('guide-ilm-output')) {
-    return;
-  }
+	if ( ! document.body.classList.contains( 'guide-ilm-output' ) ) {
+		return;
+	}
 
-  const output = document.querySelector('[class="ilm-output-name"]').textContent;
-  const element = document.querySelector('[class="ilm-element-name"]').textContent;
+	const output = document.querySelector( '[class="ilm-output-name"]' ).textContent;
+	const element = document.querySelector( '[class="ilm-element-name"]' ).textContent;
 
-  function trackGuideToolClick(event) {
-    const tool = this.textContent;
-    plausible('ToolClick', {props: {output: output, element: element, tool: tool}});
-  }
+	function trackGuideToolClick( event ) {
+		const tool = this.textContent;
+		plausible( 'ToolClick', {props: {output: output, element: element, tool: tool}} );
+	}
 
-  document.querySelectorAll('.tool a').forEach((clickedTool) => {
-    clickedTool.onclick = null;
-    clickedTool.addEventListener('click', trackGuideToolClick);
-  })
+	document.querySelectorAll( '.tool a' ).forEach( ( clickedTool ) => {
+		clickedTool.onclick = null;
+		clickedTool.addEventListener( 'click', trackGuideToolClick );
+	} );
 }
