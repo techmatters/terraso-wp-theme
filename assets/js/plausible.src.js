c58542d6adc3735560102518787ca1615984c7ae
Paul Schreiber <paulschreiber@gmail.com>
2022-12-20 15:15:13 -0500

    chore: compile js

---
+++ assets/js/plausible.src.js
@@ -0,0 +1,22 @@
+document.addEventListener( 'DOMContentLoaded', addPlausibleTracking );
+
+function addPlausibleTracking() {
+	if ( ! document.body.classList.contains( 'guide-ilm-output' ) ) {
+		return;
+	}
+
+	const output = document.querySelector( '[class="ilm-output-name"]' ).textContent;
+	const element = document.querySelector( '[class="ilm-element-name"]' ).textContent;
+
+	function trackGuideToolClick( event ) {
+		const tool = this.textContent;
+		plausible( 'ToolClick', {props: {output: output, element: element, tool: tool}} );
+	}
+
+	document.querySelectorAll( '.tool a' ).forEach( ( clickedTool ) => {
+		clickedTool.onclick = null;
+		clickedTool.addEventListener( 'click', trackGuideToolClick );
+	} );
+}
+
+//# sourceMappingURL=plausible.src.js.map
