8c09e2a49236a9842e9b486b74b8223cba453a8a
Paul Schreiber <paul@techmatters.org>
2023-04-20 16:08:15 -0400

    chore: remove I:M guidef references

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  * @since   1.0.0
  */
 
-define( 'THEME_VERSION', '1.0.1' );
+define( 'THEME_VERSION', '1.0.2' );
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	require_once __DIR__ . '/includes/class-terraso-cli.php';
