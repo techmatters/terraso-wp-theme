f9102002b7000676ea1c12dcd0933d64c686db05
Paul Schreiber <paul@techmatters.org>
2023-05-08 15:47:49 -0400

    chore: bump version to 1.0.3

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  * @since   1.0.0
  */
 
-define( 'THEME_VERSION', '1.0.2' );
+define( 'THEME_VERSION', '1.0.3' );
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	require_once __DIR__ . '/includes/class-terraso-cli.php';
