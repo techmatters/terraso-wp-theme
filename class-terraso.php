ffa55162bba39068afc5f25ff078d1d49a41326d
Paul Schreiber <paul@techmatters.org>
2023-05-12 12:03:48 -0400

    refactor: move CPT to its own class (#50)

--- class-terraso.php
+++ class-terraso.php
@@ -23,7 +23,6 @@ class Terraso {
 		add_action( 'wp_head', [ __CLASS__, 'meta_tags' ] );
 		add_action( 'after_setup_theme', [ __CLASS__, 'setup' ] );
 		add_filter( 'document_title_separator', [ __CLASS__, 'document_title_separator' ] );
-		add_action( 'init', [ __CLASS__, 'register_post_type' ] );
 		add_action( 'init', [ __CLASS__, 'help_rewrite' ] );
 		add_filter( 'auto_update_translation', '__return_true' );
 		add_filter( 'body_class', [ __CLASS__, 'filter_body_class' ] );
@@ -118,49 +117,6 @@ class Terraso {
 	 */
 	public static function document_title_separator() {
 		return '|';
-	}
-
-	/**
-	 * Register Help page post type.
-	 */
-	public static function register_post_type() {
-		$labels = [
-			'name'          => esc_html__( 'Help pages', 'terraso' ),
-			'singular_name' => esc_html__( 'Help page', 'terraso' ),
-		];
-
-		$args = [
-			'label'                 => esc_html__( 'Help pages', 'terraso' ),
-			'labels'                => $labels,
-			'description'           => '',
-			'public'                => true,
-			'publicly_queryable'    => true,
-			'show_ui'               => true,
-			'show_in_rest'          => true,
-			'rest_base'             => '',
-			'rest_controller_class' => 'WP_REST_Posts_Controller',
-			'rest_namespace'        => 'wp/v2',
-			'has_archive'           => false,
-			'show_in_menu'          => true,
-			'show_in_nav_menus'     => true,
-			'delete_with_user'      => false,
-			'exclude_from_search'   => false,
-			'capability_type'       => 'post',
-			'map_meta_cap'          => true,
-			'hierarchical'          => false,
-			'can_export'            => false,
-			'rewrite'               => [
-				'slug'       => 'help',
-				'with_front' => true,
-			],
-			'query_var'             => true,
-			'supports'              => [ 'title', 'editor', 'thumbnail', 'revisions' ],
-			'show_in_graphql'       => false,
-			'menu_position'         => 20,
-			'menu_icon'             => 'dashicons-editor-help',
-		];
-
-		register_post_type( 'help', $args );
 	}
 
 	/**
---
+++ includes/class-terraso-help-cpt.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Help Pages CPT for Terraso
+ *
+ * @package Terraso
+ */
+
+/**
+ * Holds methods for Help Custom Post Type
+ * Class Terraso_Help_CPT
+ */
+class Terraso_Help_CPT {
+
+	/**
+	 * Add actions and filters.
+	 */
+	public static function hooks() {
+		add_action( 'init', [ __CLASS__, 'register_post_type' ] );
+	}
+
+	/**
+	 * Register Help page post type.
+	 */
+	public static function register_post_type() {
+		$labels = [
+			'name'          => esc_html__( 'Help pages', 'terraso' ),
+			'singular_name' => esc_html__( 'Help page', 'terraso' ),
+		];
+
+		$args = [
+			'label'                 => esc_html__( 'Help pages', 'terraso' ),
+			'labels'                => $labels,
+			'description'           => '',
+			'public'                => true,
+			'publicly_queryable'    => true,
+			'show_ui'               => true,
+			'show_in_rest'          => true,
+			'rest_base'             => '',
+			'rest_controller_class' => 'WP_REST_Posts_Controller',
+			'rest_namespace'        => 'wp/v2',
+			'has_archive'           => false,
+			'show_in_menu'          => true,
+			'show_in_nav_menus'     => true,
+			'delete_with_user'      => false,
+			'exclude_from_search'   => false,
+			'capability_type'       => 'post',
+			'map_meta_cap'          => true,
+			'hierarchical'          => false,
+			'can_export'            => false,
+			'rewrite'               => [
+				'slug'       => 'help',
+				'with_front' => true,
+			],
+			'query_var'             => true,
+			'supports'              => [ 'title', 'editor', 'thumbnail', 'revisions' ],
+			'show_in_graphql'       => false,
+			'menu_position'         => 20,
+			'menu_icon'             => 'dashicons-editor-help',
+		];
+
+		register_post_type( 'help', $args );
+	}
+}
+
+add_action( 'after_setup_theme', [ 'Terraso_Help_CPT', 'hooks' ] );
